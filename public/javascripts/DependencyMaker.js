ae438a17523547bafb3c0eb0ffe7da7debcf1dd2
Nik Silver <nik@niksilver.com>
2015-03-29 18:28:54 +0100

    Dependency lines are now curved

--- public/javascripts/DependencyMaker.js
+++ public/javascripts/DependencyMaker.js
@@ -18,11 +18,14 @@ CriticalSight.DependencyMaker = function(sizer) {
         var endX = toElt.connector.toX;
         var endY = toElt.connector.toY;
         
+        var controlX = (startX == endX) ? (startX + sizer.unitWidth/5) : endX;
+        var controlY = (startX == endX) ? (startY + (endY - startY)/2) : startY;
+        
         var arrow = new createjs.Shape();
         arrow.graphics.beginStroke('rgb(0,0,0)'). // Line colour
             setStrokeStyle(2). // Line width
             moveTo(startX, startY).
-            lineTo(endX, endY);
+            quadraticCurveTo(controlX, controlY, endX, endY);
         CriticalSight.Util.setBounds(arrow,
                 [startX], [startY], [endX - startX], [endY - startY]);
         return arrow;
