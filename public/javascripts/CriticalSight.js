9765cf4bdfbebf86e28f0beeba8852689c451786
Nik Silver <nik@niksilver.com>
2015-07-23 22:19:00 +0100

    Converted a proper test to run with karma and require.js.

--- public/javascripts/CriticalSight.js
+++ public/javascripts/CriticalSight.js
@@ -3,43 +3,51 @@
  * plus sundry definitions.
  */
 
-var CriticalSight = {};
-
-CriticalSight.Util = {
-	/**
-	 * In array `arr` require each object to have a property `propName`
-	 * or else throw the error message `msg`.
-	 */
-	forEachRequireProperty : function(arr, propName, msg) {
-		arr.forEach(function(item) {
-			if (typeof item[propName] === 'undefined') {
-				throw new CriticalSight.BadlyDefinedObjectError(msg);
+define(function() {
+	return {
+	    Util: {
+			/**
+			 * In array `arr` require each object to have a property `propName`
+			 * or else throw the error message `msg`.
+			 */
+			forEachRequireProperty : function(arr, propName, msg) {
+				arr.forEach(function(item) {
+					if (typeof item[propName] === 'undefined') {
+						throw new CriticalSight.BadlyDefinedObjectError(msg);
+					}
+				});
+			},
+			
+			/**
+			 * Set the bounds of a shape. We give a list of left coords,
+			 * top coords (and it picks the smallest of each), widths
+			 * and heights (and it picks the greatest of each).
+			 */
+			setBounds: function(shape, lefts, tops, widths, heights) {
+			    shape.setBounds(
+			            Math.min.apply(this, lefts),
+			            Math.min.apply(this, tops),
+			            Math.max.apply(this, widths),
+			            Math.max.apply(this, heights));
 			}
-		});
-	},
-	
-	/**
-	 * Set the bounds of a shape. We give a list of left coords,
-	 * top coords (and it picks the smallest of each), widths
-	 * and heights (and it picks the greatest of each).
-	 */
-	setBounds: function(shape, lefts, tops, widths, heights) {
-	    shape.setBounds(
-	            Math.min.apply(this, lefts),
-	            Math.min.apply(this, tops),
-	            Math.max.apply(this, widths),
-	            Math.max.apply(this, heights));
-	}
-};
-
-CriticalSight.UndefinedTaskError = function(message) {
-    this.name = "UndefinedTaskError";
-    this.message = (message || "");
-};
-CriticalSight.UndefinedTaskError.prototype = Error.prototype;
-
-CriticalSight.BadlyDefinedObjectError = function(message) {
-    this.name = "BadlyDefinedObjectError";
-    this.message = (message || "");
-};
-CriticalSight.BadlyDefinedObjectError.prototype = Error.prototype;
+		},
+		
+		UndefinedTaskError: (function() {
+			var fn = function(message) {
+				this.name = "UndefinedTaskError";
+				this.message = (message || "");
+			};
+			fn.prototype = Error.prototype;
+			return fn;
+		})(),
+		
+		BadlyDefinedObjectError: (function() {
+			var fn = function(message) {
+				this.name = "BadlyDefinedObjectError";
+				this.message = (message || "");
+			};
+			fn.prototype = Error.prototype;
+			return fn;
+		})()
+	};
+});
--- test/javascripts/test-main.js
+++ test/javascripts/test-main.js
@@ -3,7 +3,7 @@
 // https://github.com/kjbekkelund/karma-requirejs
 // for a complete, small, working system.
 //
-var TEST_REGEXP = /DemoTest.js$/i;
+var TEST_REGEXP = /DemoTest.js|UtilTest.js$/i;
 var allTestFiles = [];
 
 // Get a list of all the test files to include
@@ -18,10 +18,19 @@ require.config({
   // which is the basePath from your config file
   baseUrl: '/base/public/javascripts',
 
-  // example of using a path translations (paths), to allow us to refer
+  // We are using path translations (paths), to allow us to refer
   // to different library dependencies, without using relative paths
   paths: {
-    'jquery': '../lib/jquery',
+	'jquery': '../lib/jquery',
+	'easeljs': '../lib/easeljs-0.8.0.min'
+  },
+  
+  // We use a shim for easeljs because it's not written as a module
+  // and it drop a createjs variable into the global namespace.
+  shim: {
+	'easeljs': {
+	  exports: 'createjs'
+	}
   },
 
   // dynamically load all test files
