a3bf5aa3c4a4284060bea1a4e516a04c918f561e
Nik Silver <nik@niksilver.com>
2015-07-24 19:40:15 +0100

    Migrated PeriodMaker and PeriodMakerTest to require.js and karma

--- public/javascripts/PeriodMaker.js
+++ public/javascripts/PeriodMaker.js
@@ -5,94 +5,96 @@
  * connector: {fromX: 99, fromY: 99, toX: 99, toY: 99}.
  */
 
-CriticalSight.PeriodMaker = function(sizer) {
-	'use strict';
+define(['Util'], function(Util) {
+	return function(sizer) {
+		'use strict';
+		
+		this.sizer = sizer;
+		
+		var taskStrokeColour = 'rgb(0,0,255)';
+		var taskFillColour = 'rgb(128,128,255)';
+		var taskStrokeWidth = 2;
+		
+		var bufferStrokeColour = 'rgb(64,64,64)';
+		var bufferFillColour = 'rgb(192,192,192)';
+		var bufferStrokeWidth = 2;
+		
+		/**
+		 * A shape for a period (depends if it's zero-length or not).
+		 * `type` should be "task" or "buffer".
+		 * `idx` is the row index on the chart.
+		 */
+		this.periodShape = function(type, idx, start, duration) {
+			if (type == "buffer") {
+				return this.bufferRect(idx, start, duration);
+			} else if (duration === 0 ) {
+				return this.taskDiamond(idx, start);
+			} else {
+				return this.taskRect(idx, start, duration);
+			}
+		};
+		
+		/**
+		 * A diamond-shaped task, if zero-duration.
+		 */
+		this.taskDiamond = function(idx, start) {
+		    var startX = sizer.left(start);
+		    var startY = sizer.top(idx);
+	        var halfHeight = sizer.unitHeight / 2;
+	        var halfWidth = halfHeight;
 	
-	this.sizer = sizer;
-	
-	var taskStrokeColour = 'rgb(0,0,255)';
-	var taskFillColour = 'rgb(128,128,255)';
-	var taskStrokeWidth = 2;
-	
-	var bufferStrokeColour = 'rgb(64,64,64)';
-	var bufferFillColour = 'rgb(192,192,192)';
-	var bufferStrokeWidth = 2;
-	
-	/**
-	 * A shape for a period (depends if it's zero-length or not).
-	 * `type` should be "task" or "buffer".
-	 * `idx` is the row index on the chart.
-	 */
-	this.periodShape = function(type, idx, start, duration) {
-		if (type == "buffer") {
-			return this.bufferRect(idx, start, duration);
-		} else if (duration === 0 ) {
-			return this.taskDiamond(idx, start);
-		} else {
-			return this.taskRect(idx, start, duration);
-		}
+	        var diamond = new createjs.Shape();
+		    diamond.graphics.beginFill(taskFillColour).
+		        beginStroke(taskStrokeColour).
+		        setStrokeStyle(taskStrokeWidth).
+		        moveTo(startX, startY).
+		        lineTo(startX + halfWidth, startY + halfHeight).
+		        lineTo(startX, startY + 2*halfHeight).
+		        lineTo(startX - halfWidth, startY + halfHeight).
+		        closePath();
+		    Util.setBounds(diamond,
+		            [startX - halfWidth], [startY], [2*halfWidth], [2*halfHeight]);
+		    diamond.set({ connector: {
+	    	        fromX: startX + halfWidth,
+	    	        fromY: startY + halfHeight,
+	    	        toX: startX,
+	    	        toY: startY
+		    }});
+		    return diamond;
+		};
+		
+		/**
+		 * A rectangular task.
+		 */
+		this.taskRect = function(idx, start, duration) {
+		    return this._rect(idx, start, duration, taskFillColour, taskStrokeColour, taskStrokeWidth);
+		};
+		
+		/**
+		 * A buffer
+		 */
+		this.bufferRect = function(idx, start, duration) {
+	        return this._rect(idx, start, duration, bufferFillColour, bufferStrokeColour, bufferStrokeWidth);
+		};
+	    
+	    this._rect = function(idx, start, duration, fillColour, strokeColour, strokeWidth) {
+	        var rect = new createjs.Shape();
+	        var left = sizer.left(start);
+	        var top = sizer.top(idx);
+	        var width = sizer.width(duration);
+	        var height = sizer.unitHeight;
+	        rect.graphics.beginFill(fillColour).
+	            beginStroke(strokeColour).
+	            setStrokeStyle(strokeWidth).
+	            drawRect(left, top, width, height);
+	        Util.setBounds(rect, [left], [top], [width], [height]);
+	        rect.set({ connector: {
+	                fromX: left + width,
+	                fromY: top + height/2,
+	                toX: left + sizer.unitWidth/5,
+	                toY: top
+	        }});
+	        return rect;
+	    };
 	};
-	
-	/**
-	 * A diamond-shaped task, if zero-duration.
-	 */
-	this.taskDiamond = function(idx, start) {
-	    var startX = sizer.left(start);
-	    var startY = sizer.top(idx);
-        var halfHeight = sizer.unitHeight / 2;
-        var halfWidth = halfHeight;
-
-        var diamond = new createjs.Shape();
-	    diamond.graphics.beginFill(taskFillColour).
-	        beginStroke(taskStrokeColour).
-	        setStrokeStyle(taskStrokeWidth).
-	        moveTo(startX, startY).
-	        lineTo(startX + halfWidth, startY + halfHeight).
-	        lineTo(startX, startY + 2*halfHeight).
-	        lineTo(startX - halfWidth, startY + halfHeight).
-	        closePath();
-	    CriticalSight.Util.setBounds(diamond,
-	            [startX - halfWidth], [startY], [2*halfWidth], [2*halfHeight]);
-	    diamond.set({ connector: {
-    	        fromX: startX + halfWidth,
-    	        fromY: startY + halfHeight,
-    	        toX: startX,
-    	        toY: startY
-	    }});
-	    return diamond;
-	};
-	
-	/**
-	 * A rectangular task.
-	 */
-	this.taskRect = function(idx, start, duration) {
-	    return this._rect(idx, start, duration, taskFillColour, taskStrokeColour, taskStrokeWidth);
-	};
-	
-	/**
-	 * A buffer
-	 */
-	this.bufferRect = function(idx, start, duration) {
-        return this._rect(idx, start, duration, bufferFillColour, bufferStrokeColour, bufferStrokeWidth);
-	};
-    
-    this._rect = function(idx, start, duration, fillColour, strokeColour, strokeWidth) {
-        var rect = new createjs.Shape();
-        var left = sizer.left(start);
-        var top = sizer.top(idx);
-        var width = sizer.width(duration);
-        var height = sizer.unitHeight;
-        rect.graphics.beginFill(fillColour).
-            beginStroke(strokeColour).
-            setStrokeStyle(strokeWidth).
-            drawRect(left, top, width, height);
-        CriticalSight.Util.setBounds(rect, [left], [top], [width], [height]);
-        rect.set({ connector: {
-                fromX: left + width,
-                fromY: top + height/2,
-                toX: left + sizer.unitWidth/5,
-                toY: top
-        }});
-        return rect;
-    };
-};
+});
--- test/javascripts/PeriodMakerTest.js
+++ test/javascripts/PeriodMakerTest.js
@@ -1,58 +1,58 @@
-describe("PeriodMaker", function() {
-    'use strict';
-    
-    var CS = CriticalSight;
-    
-    describe("taskDiamond", function() {
-        it("should add a connector point correctly", function() {
-            var plan = CS.TestPlans.dummyPlan(44, 55);
-            var sizer = new CS.Sizer(plan, 50, 10);
-            var pMaker = new CS.PeriodMaker(sizer);
-            
-            var idx = 3;
-            var start = 56;
-            var diamond = pMaker.taskDiamond(idx, start);
-            
-            expect( diamond.connector.fromX ).toEqual( diamond.getBounds().x + diamond.getBounds().width );
-            expect( diamond.connector.fromY ).toEqual( sizer.top(idx) + sizer.unitHeight/2 );
-            expect( diamond.connector.toX ).toEqual( sizer.left(start) );
-            expect( diamond.connector.toY ).toEqual( sizer.top(idx) );
-        });
-    });
-    
-    describe("taskRect", function() {
-        it("should add a connector point correctly", function() {
-            var plan = CS.TestPlans.dummyPlan(44, 55);
-            var sizer = new CS.Sizer(plan, 50, 10);
-            var pMaker = new CS.PeriodMaker(sizer);
-            
-            var idx = 3;
-            var start = 56;
-            var duration = 4;
-            var rect = pMaker.taskRect(idx, start, duration);
-            
-            expect( rect.connector.fromX ).toEqual( sizer.left(start) + sizer.width(duration) );
-            expect( rect.connector.fromY ).toEqual( sizer.top(idx) + sizer.unitHeight/2 );
-            expect( rect.connector.toX ).toEqual( sizer.left(start) + sizer.width(1)/5 );
-            expect( rect.connector.toY ).toEqual( sizer.top(idx) );
-        });
-    });
-    
-    describe("bufferRect", function() {
-        it("should add a connector point correctly", function() {
-            var plan = CS.TestPlans.dummyPlan(44, 55);
-            var sizer = new CS.Sizer(plan, 50, 10);
-            var pMaker = new CS.PeriodMaker(sizer);
-            
-            var idx = 3;
-            var start = 56;
-            var duration = 4;
-            var rect = pMaker.bufferRect(idx, start, duration);
-            
-            expect( rect.connector.fromX ).toEqual( sizer.left(start) + sizer.width(duration) );
-            expect( rect.connector.fromY ).toEqual( sizer.top(idx) + sizer.unitHeight/2 );
-            expect( rect.connector.toX ).toEqual( sizer.left(start) + sizer.width(1)/5 );
-            expect( rect.connector.toY ).toEqual( sizer.top(idx) );
-        });
-    });
+define(['PeriodMaker', 'Sizer', 'test/TestPlans'], function(PeriodMaker, Sizer, TestPlans) {
+	describe("PeriodMaker", function() {
+	    'use strict';
+	    
+	    describe("taskDiamond", function() {
+	        it("should add a connector point correctly", function() {
+	            var plan = TestPlans.dummyPlan(44, 55);
+	            var sizer = new Sizer(plan, 50, 10);
+	            var pMaker = new PeriodMaker(sizer);
+	            
+	            var idx = 3;
+	            var start = 56;
+	            var diamond = pMaker.taskDiamond(idx, start);
+	            
+	            expect( diamond.connector.fromX ).toEqual( diamond.getBounds().x + diamond.getBounds().width );
+	            expect( diamond.connector.fromY ).toEqual( sizer.top(idx) + sizer.unitHeight/2 );
+	            expect( diamond.connector.toX ).toEqual( sizer.left(start) );
+	            expect( diamond.connector.toY ).toEqual( sizer.top(idx) );
+	        });
+	    });
+	    
+	    describe("taskRect", function() {
+	        it("should add a connector point correctly", function() {
+	            var plan = TestPlans.dummyPlan(44, 55);
+	            var sizer = new Sizer(plan, 50, 10);
+	            var pMaker = new PeriodMaker(sizer);
+	            
+	            var idx = 3;
+	            var start = 56;
+	            var duration = 4;
+	            var rect = pMaker.taskRect(idx, start, duration);
+	            
+	            expect( rect.connector.fromX ).toEqual( sizer.left(start) + sizer.width(duration) );
+	            expect( rect.connector.fromY ).toEqual( sizer.top(idx) + sizer.unitHeight/2 );
+	            expect( rect.connector.toX ).toEqual( sizer.left(start) + sizer.width(1)/5 );
+	            expect( rect.connector.toY ).toEqual( sizer.top(idx) );
+	        });
+	    });
+	    
+	    describe("bufferRect", function() {
+	        it("should add a connector point correctly", function() {
+	            var plan = TestPlans.dummyPlan(44, 55);
+	            var sizer = new Sizer(plan, 50, 10);
+	            var pMaker = new PeriodMaker(sizer);
+	            
+	            var idx = 3;
+	            var start = 56;
+	            var duration = 4;
+	            var rect = pMaker.bufferRect(idx, start, duration);
+	            
+	            expect( rect.connector.fromX ).toEqual( sizer.left(start) + sizer.width(duration) );
+	            expect( rect.connector.fromY ).toEqual( sizer.top(idx) + sizer.unitHeight/2 );
+	            expect( rect.connector.toX ).toEqual( sizer.left(start) + sizer.width(1)/5 );
+	            expect( rect.connector.toY ).toEqual( sizer.top(idx) );
+	        });
+	    });
+	});
 });
