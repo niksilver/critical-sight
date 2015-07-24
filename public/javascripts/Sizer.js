8586d2f3dab87643198739a06134a89a6b6d434f
Nik Silver <nik@niksilver.com>
2015-07-24 19:32:34 +0100

    Migrated Sizer and SizerTest to require.js and karma

--- public/javascripts/Sizer.js
+++ public/javascripts/Sizer.js
@@ -3,49 +3,49 @@
  * @param planStart   The time at which the plan starts (the earliest possible time)
  */
 
-CriticalSight.Sizer = function(plan, unitWidth, unitHeight) {
-    'use strict';
-    
-    var CS = CriticalSight;
-    
-    if (arguments.length !== 3)
-        throw new Error("Expected 3 arguments, but got " + arguments.length);
-    if (!(plan instanceof CS.Plan))
-        throw new Error("Expected first argument (plan) to be an object, but it wasn't");
-    
-    var planStart = plan.start;
-    var periodCount = plan.periodList.length;
-
-    this.unitHeight = unitHeight;
-    this.unitWidth = unitWidth;
-    
-    this.topPadding = unitHeight * 0.5;
-    this.midPadding = unitHeight * 0.5;
-    this.bottomPadding = unitHeight * 0.5;
-    
-    this.chartHeight = this.topPadding +
-            (periodCount-1) * this.midPadding +
-            this.bottomPadding +
-            periodCount * this.unitHeight;
-
-    /**
-     * Top of a task rectangle, where the task is index `idx`.
-     */
-    this.top = function(idx) {
-        return (this.unitHeight + this.midPadding) * idx + this.topPadding;
-    };
-
-    /**
-     * Left of a task rectangle, where the start is at time `start`.
-     */
-    this.left = function(start) {
-        return (start - planStart) * this.unitWidth;
-    };
-
-    /**
-     * Width of a task rectangle, where the task's duration is `dur`.
-     */
-    this.width = function(dur) {
-        return dur * this.unitWidth;
-    };
-};
+define(['Plan'], function(Plan) {
+	return function(plan, unitWidth, unitHeight) {
+	    'use strict';
+	    
+	    if (arguments.length !== 3)
+	        throw new Error("Expected 3 arguments, but got " + arguments.length);
+	    if (!(plan instanceof Plan))
+	        throw new Error("Expected first argument (plan) to be an object, but it wasn't");
+	    
+	    var planStart = plan.start;
+	    var periodCount = plan.periodList.length;
+	
+	    this.unitHeight = unitHeight;
+	    this.unitWidth = unitWidth;
+	    
+	    this.topPadding = unitHeight * 0.5;
+	    this.midPadding = unitHeight * 0.5;
+	    this.bottomPadding = unitHeight * 0.5;
+	    
+	    this.chartHeight = this.topPadding +
+	            (periodCount-1) * this.midPadding +
+	            this.bottomPadding +
+	            periodCount * this.unitHeight;
+	
+	    /**
+	     * Top of a task rectangle, where the task is index `idx`.
+	     */
+	    this.top = function(idx) {
+	        return (this.unitHeight + this.midPadding) * idx + this.topPadding;
+	    };
+	
+	    /**
+	     * Left of a task rectangle, where the start is at time `start`.
+	     */
+	    this.left = function(start) {
+	        return (start - planStart) * this.unitWidth;
+	    };
+	
+	    /**
+	     * Width of a task rectangle, where the task's duration is `dur`.
+	     */
+	    this.width = function(dur) {
+	        return dur * this.unitWidth;
+	    };
+	};
+});
--- test/javascripts/SizerTest.js
+++ test/javascripts/SizerTest.js
@@ -2,55 +2,55 @@
  * Tests for the Sizer prototype
  */
 
-describe("Sizer", function() {
-	
-	var CS = CriticalSight;
-	
-	describe("constructorFn", function() {
-	    it("should reject the first argument if it's a number", function() {
-	        expect( function(){ new CS.Sizer(11, 22, 33); } ).toThrow();
-	    });
-	});
-	
-	describe("top", function() {
-    	it("should position the top of the zero-indexed task correctly", function() {
-            var plan = CS.TestPlans.dummyPlan(1, 0);
-    		var s = new CS.Sizer(plan, 999, 10);
-    		expect( s.top(0) ).toEqual( 10/2 );
-    	});
-    	
-    	it("should position the top of the 1-indexed task correctly", function() {
-            var plan = CS.TestPlans.dummyPlan(1, 0);
-    		var s = new CS.Sizer(plan, 999, 10);
-    		expect( s.top(1) ).toEqual( 10/2 + 10 + 10/2 );
-    	});
-	});
-	
-	describe("left", function() {
-    	it("should position the left of a task mindful of the plan start time", function() {
-    		// First create a zero-start sizer and take a measurement from that.
-    		// Them create a non-zero-start sizer and compare
-    	    
-            var plan0 = CS.TestPlans.dummyPlan(1, 0);
-    		var s0 = new CS.Sizer(plan0, 999, 10);
-    		var left0 = s0.left(0);
-    		
-            var plan35 = CS.TestPlans.dummyPlan(1, 35);
-    		var s35 = new CS.Sizer(plan35, 999, 10);
-    		var left35 = s35.left(35);
-    		expect( left35 ).toEqual( left0 );
-    	});
-	});
-	
-	describe("chartHeight", function() {
-	    it("should calculate the chart height correctly for several tasks", function() {
-	        var plan = CS.TestPlans.dummyPlan(5, 999);
-	        var s = new CS.Sizer(plan, 888, 11);
-	        
-	        var totalBars = 5 * 11;
-	        var totalPadding = s.topPadding + (5-1)*s.midPadding + s.bottomPadding;
-	        
-	        expect( s.chartHeight ).toEqual( totalBars +totalPadding );
-	    });
+define(['Sizer', 'test/TestPlans'], function(Sizer, TestPlans) {
+	describe("Sizer", function() {
+		
+		describe("constructorFn", function() {
+		    it("should reject the first argument if it's a number", function() {
+		        expect( function(){ new Sizer(11, 22, 33); } ).toThrow();
+		    });
+		});
+		
+		describe("top", function() {
+	    	it("should position the top of the zero-indexed task correctly", function() {
+	            var plan = TestPlans.dummyPlan(1, 0);
+	    		var s = new Sizer(plan, 999, 10);
+	    		expect( s.top(0) ).toEqual( 10/2 );
+	    	});
+	    	
+	    	it("should position the top of the 1-indexed task correctly", function() {
+	            var plan = TestPlans.dummyPlan(1, 0);
+	    		var s = new Sizer(plan, 999, 10);
+	    		expect( s.top(1) ).toEqual( 10/2 + 10 + 10/2 );
+	    	});
+		});
+		
+		describe("left", function() {
+	    	it("should position the left of a task mindful of the plan start time", function() {
+	    		// First create a zero-start sizer and take a measurement from that.
+	    		// Them create a non-zero-start sizer and compare
+	    	    
+	            var plan0 = TestPlans.dummyPlan(1, 0);
+	    		var s0 = new Sizer(plan0, 999, 10);
+	    		var left0 = s0.left(0);
+	    		
+	            var plan35 = TestPlans.dummyPlan(1, 35);
+	    		var s35 = new Sizer(plan35, 999, 10);
+	    		var left35 = s35.left(35);
+	    		expect( left35 ).toEqual( left0 );
+	    	});
+		});
+		
+		describe("chartHeight", function() {
+		    it("should calculate the chart height correctly for several tasks", function() {
+		        var plan = TestPlans.dummyPlan(5, 999);
+		        var s = new Sizer(plan, 888, 11);
+		        
+		        var totalBars = 5 * 11;
+		        var totalPadding = s.topPadding + (5-1)*s.midPadding + s.bottomPadding;
+		        
+		        expect( s.chartHeight ).toEqual( totalBars +totalPadding );
+		    });
+		});
 	});
 });
