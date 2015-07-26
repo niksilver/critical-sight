79a73e133e50a5ee3d5ef03c656a926dd3537a03
Nik Silver <nik@niksilver.com>
2015-07-27 00:23:31 +0100

    Main application now works with require.js.

--- public/javascripts/Chart.js
+++ public/javascripts/Chart.js
@@ -2,61 +2,63 @@
  * Demo of a Gantt chart
  */
 
-CriticalSight.Chart = {
-        
-    /**
-     * Show the chart bar area defined by the Json `spec` on the specified `stage`.
-     */
-	"showBarsByJson" : function(stage, spec) {
-		
-		var CS = CriticalSight;
-		
-		// Clear any old elements from the last version of the chart.
-        stage.removeAllChildren();
-        stage.update();
-
-		var plan = new CS.Plan(spec);
-		
-		var htmlCanvasWidth = stage.canvas.width;
-		var sizer = new CS.Sizer(plan, htmlCanvasWidth / plan.duration, 20);
-		var pMaker = new CS.PeriodMaker(sizer);
-		var dMaker = new CS.DependencyMaker(sizer);
-		
-		stage.canvas.height = sizer.chartHeight;
-
-		var periodShapes = {};
-		for (var i = 0; i < plan.periodList.length; i++) {
-			var period = plan.periodList[i];
-			var id = period.id;
-			var type = period.type;
-			var start = period.start;
-			var duration = period.duration;
-			var periodShape = pMaker.periodShape(type, i, start, duration);
-			stage.addChild(periodShape);
-			periodShapes[id] = periodShape;
-		}
-		
-		var deps = plan.dependencyIDs;
-		for (i = 0; i < deps.length; i++) {
-		    var fromId = deps[i][0];
-		    var toId = deps[i][1];
-		    var fromShape = periodShapes[fromId];
-		    var toShape = periodShapes[toId];
-		    var depShape = dMaker.dependency(fromShape, toShape);
-		    stage.addChild(depShape);
-		}
-		stage.update();
-	},
+define(['Plan', 'Sizer', 'PeriodMaker', 'DependencyMaker', 'jquery'],
+		function(Plan, Sizer, PeriodMaker, DependencyMaker, $) {
+	var self =  {
+	        
+	    /**
+	     * Show the chart bar area defined by the Json `spec` on the specified `stage`.
+	     */
+		"showBarsByJson" : function(stage, spec) {
+			
+			// Clear any old elements from the last version of the chart.
+	        stage.removeAllChildren();
+	        stage.update();
 	
-	/**
-	 * Show the chart bar area defined by the text description, on the specified `stage
-	 */
-	"showBarsByText" : function(stage, text) {
-        $.post('/readPlan',
-                "text=" + encodeURI(text),
-                function(spec) {
-                    CS.Chart.showBarsByJson(stage, spec);
-                });
-
-	}
-};
+			var plan = new Plan(spec);
+			
+			var htmlCanvasWidth = stage.canvas.width;
+			var sizer = new Sizer(plan, htmlCanvasWidth / plan.duration, 20);
+			var pMaker = new PeriodMaker(sizer);
+			var dMaker = new DependencyMaker(sizer);
+			
+			stage.canvas.height = sizer.chartHeight;
+	
+			var periodShapes = {};
+			for (var i = 0; i < plan.periodList.length; i++) {
+				var period = plan.periodList[i];
+				var id = period.id;
+				var type = period.type;
+				var start = period.start;
+				var duration = period.duration;
+				var periodShape = pMaker.periodShape(type, i, start, duration);
+				stage.addChild(periodShape);
+				periodShapes[id] = periodShape;
+			}
+			
+			var deps = plan.dependencyIDs;
+			for (i = 0; i < deps.length; i++) {
+			    var fromId = deps[i][0];
+			    var toId = deps[i][1];
+			    var fromShape = periodShapes[fromId];
+			    var toShape = periodShapes[toId];
+			    var depShape = dMaker.dependency(fromShape, toShape);
+			    stage.addChild(depShape);
+			}
+			stage.update();
+		},
+		
+		/**
+		 * Show the chart bar area defined by the text description, on the specified `stage
+		 */
+		"showBarsByText" : function(stage, text) {
+	        $.post('/readPlan',
+	                "text=" + encodeURI(text),
+	                function(spec) {
+	                    self.showBarsByJson(stage, spec);
+	                });
+	
+		}
+	};
+	return self;
+});
--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -1,55 +1,76 @@
 /**
  * Code for the demo.html page, just for a quick visual test.
  */
-var CS = CriticalSight;
 
-var defaultPlan =
-    't0: "Start"\n' +
-    't1: "Write a long list" 2.0\n' +
-    't2: "Do what\'s on the list" 5.0\n' +
-    'tEnd: "End"\n' +
-    't4: "Extra work" 2.0\n' +
-    't0 -> t1 -> tEnd\n' +
-    't0 -> t2 -> tEnd\n' +
-    't0 -> t4 -> t1\n' +
-    't4 -> tEnd\n';
+require.config({
+    // Default location for any module IDs
+    baseUrl: '/public/javascripts',
+    
+    // Each library is is its own location
+    paths: {
+    	'jquery': '../lib/jquery',
+    	'easeljs': '../lib/easeljs-0.8.0.min'
+      },
+      
+    // We use a shim for easeljs because it's not written as a module
+    // and it drop a createjs variable into the global namespace.
+    shim: {
+      'easeljs': {
+        exports: 'createjs'
+      }
+    }
 
-// Do this when the page has loaded
+});
 
-$(function() {
-
-	var htmlCanvas = document.getElementById('chart-bars-canvas');
-	htmlCanvas.width = 1000;
-	htmlCanvas.height = 500;
-
-	var stage = new createjs.Stage(htmlCanvas);
-
-	// Lay out the chart text (testing only)
+require(['Chart', 'jquery', 'easeljs'], function(Chart, $, easeljs) {
+	var defaultPlan =
+	    't0: "Start"\n' +
+	    't1: "Write a long list" 2.0\n' +
+	    't2: "Do what\'s on the list" 5.0\n' +
+	    'tEnd: "End"\n' +
+	    't4: "Extra work" 2.0\n' +
+	    't0 -> t1 -> tEnd\n' +
+	    't0 -> t2 -> tEnd\n' +
+	    't0 -> t4 -> t1\n' +
+	    't4 -> tEnd\n';
 	
-	$("#chart-text").css("padding-top", "5px");  // Top padding - (mid-bar padding / 2)
-	$("#chart-text-inner td").each(function() {
-	    $(this).css("height", "20px");        // Bar height
-	    $(this).css("padding-top", "5px");    // Mid-bar padding / 2
-	    $(this).css("padding-bottom", "5px"); // Mid-bar padding / 2
-	});
+	// Do this when the page has loaded
 	
-	// Insert and render the default plan (chart bars)
+	$(function() {
 	
-	$("#plantext").val(defaultPlan);
-	CS.Chart.showBarsByText(stage, defaultPlan);
+		var htmlCanvas = document.getElementById('chart-bars-canvas');
+		htmlCanvas.width = 1000;
+		htmlCanvas.height = 500;
 	
-	// Update the rendering when we change the text
+		var stage = new createjs.Stage(htmlCanvas);
 	
-	$("#submit-plan").click(function() {
-	    var text = $("#plantext").val();
-	    CS.Chart.showBarsByText(stage, text);
-	});
-	
-	// Respond to the bars div scrolling
-	
-	$("#chart-bars").scroll(function(data) {
-	    var top = $("#chart-bars").scrollTop();
-	    var marginTop = ""+(-1 * top)+"px";
-	    $("#chart-text-inner").css("margin-top", marginTop);
+		// Lay out the chart text (testing only)
+		
+		$("#chart-text").css("padding-top", "5px");  // Top padding - (mid-bar padding / 2)
+		$("#chart-text-inner td").each(function() {
+		    $(this).css("height", "20px");        // Bar height
+		    $(this).css("padding-top", "5px");    // Mid-bar padding / 2
+		    $(this).css("padding-bottom", "5px"); // Mid-bar padding / 2
+		});
+		
+		// Insert and render the default plan (chart bars)
+		
+		$("#plantext").val(defaultPlan);
+		Chart.showBarsByText(stage, defaultPlan);
+		
+		// Update the rendering when we change the text
+		
+		$("#submit-plan").click(function() {
+		    var text = $("#plantext").val();
+		    Chart.showBarsByText(stage, text);
+		});
+		
+		// Respond to the bars div scrolling
+		
+		$("#chart-bars").scroll(function(data) {
+		    var top = $("#chart-bars").scrollTop();
+		    var marginTop = ""+(-1 * top)+"px";
+		    $("#chart-text-inner").css("margin-top", marginTop);
+		});
 	});
 });
